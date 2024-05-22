1bb36b5f5f35c111a21c7013e6cc26deec76a3d9
TiTY <tee25450@gmail.com>
2024-05-22 14:59:16 +0700

    JS-Skill-Checkpoint#1-Q1-5(done)

--- question-2.js
+++ question-2.js
@@ -4,3 +4,12 @@ const inventory = [
   { name: "Banana", price: 10, quantity: 50 },
 ];
 // เริ่มเขียนโค้ดตรงนี้
+inventory[0].quantity = 200;
+
+inventory.push({ name: "Orange", price: 20, quantity: 300 });
+
+let result = 0;
+for (let i = 0; i < inventory.length; i = i + 1) {
+  result = result + inventory[i].price * inventory[i].quantity;
+}
+console.log(`มูลค่ารวมของจำนวนสินค้าทั้งหมดในสต็อก ${result} บาท`);
--- question-5.js
+++ question-5.js
@@ -19,3 +19,18 @@ const products = [
 
 const promotionCode = "";
 // เริ่มเขียนโค้ดตรงนี้
+let totalPrice = 0;
+const calculateTotalPrice = (products, promotionCode) => {
+  for (let i = 0; i < products.length; i = i + 1) {
+    totalPrice = totalPrice + products[i].price * products[i].quantity;
+  }
+  if (promotionCode === 50) {
+    return totalPrice - totalPrice * 0.5;
+  } else if (promotionCode === 20) {
+    return totalPrice - totalPrice * 0.2;
+  } else {
+    return totalPrice;
+  }
+};
+
+console.log(calculateTotalPrice(products, 20));
